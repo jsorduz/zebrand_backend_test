1a0f7235055bdb4a6c90f5d91f1e733fa7b153fb
“Sebastian <jorduz97@gmail.com>
2023-04-01 12:05:56 -0500

    Add pytest

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/products/")
+async def list_products():
+    return {"message": "list products"}
+
+
+@app.get("/users/")
+async def list_users():
+    return {"message": "list users"}
---
+++ tests/products/test_list_products.py
@@ -0,0 +1,13 @@
+import json
+
+from fastapi import status
+from starlette.testclient import TestClient
+
+LIST_PRODUCTS_URL = "/products/"
+
+def test_list_users(client: TestClient):
+    response = client.get(LIST_PRODUCTS_URL)
+    response_json = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response_json["message"] == "list products"
