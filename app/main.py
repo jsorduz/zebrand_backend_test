a430c870b447a909870dc49c6300b6ab48a2f29a
“Sebastian <jorduz97@gmail.com>
2023-04-01 16:17:16 -0500

    Add models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.routes.products import router as product_router
 from app.routes.users import router as user_router
 
-app = FastAPI()
-app.include_router(user_router)
-app.include_router(product_router)
+app = FastAPI(
+    openapi_url="/openapi.json",
+    docs_url="/docs",
+)
+app.include_router(user_router, tags=["users"])
+app.include_router(product_router, tags=["products"])
+
+
+register_tortoise(
+    app,
+    db_url="sqlite://:memory:",
+    modules={"models": ["app.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
---
+++ app/models.py
@@ -0,0 +1,58 @@
+from tortoise import fields
+from tortoise.models import Model
+
+VARCHAR_MAX_LENGTH = 255
+
+
+class UUIDMixin(Model):
+    id = fields.UUIDField(pk=True)
+
+    class Meta:
+        abstract = True
+
+
+class TimeStampedMixin(Model):
+    created_at = fields.DatetimeField(null=False, auto_now_add=True)
+    updated_at = fields.DatetimeField(null=False, auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(UUIDMixin, TimeStampedMixin):
+    email = fields.CharField(
+        max_length=VARCHAR_MAX_LENGTH, null=False, unique=True, index=True
+    )
+    hashed_password = fields.CharField(max_length=VARCHAR_MAX_LENGTH, null=False)
+
+    class Meta:
+        table: str = "users"
+
+    @classmethod
+    def hash_password(cls, password: str):
+        # TODO: implement a hash password algorithm
+        return f"hashed-{password}"
+
+
+class Brand(UUIDMixin, TimeStampedMixin):
+    name = fields.CharField(
+        max_length=VARCHAR_MAX_LENGTH, null=False, unique=True, index=True
+    )
+
+    class Meta:
+        table: str = "brands"
+
+
+class Product(UUIDMixin, TimeStampedMixin):
+    sku = fields.CharField(
+        max_length=VARCHAR_MAX_LENGTH, null=False, unique=True, index=True
+    )
+    name = fields.CharField(max_length=VARCHAR_MAX_LENGTH, null=False)
+    price = fields.FloatField(null=False)
+    views = fields.IntField(null=False)
+    brand = fields.ForeignKeyField(
+        "models.Brand", related_name="users", on_delete=fields.RESTRICT, null=False
+    )
+
+    class Meta:
+        table: str = "products"
